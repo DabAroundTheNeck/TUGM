feca5afc8bb5c37e36b896538e22cea13adbc3e5
Michael Blank <michi2806@gmail.com>
2017-12-05 23:08:22 +0100

    Just testing stuff

---
+++ MichiSampleGame/MichiSampleGame/Programm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using osu.Framework;
+using osu.Framework.Platform;
+
+namespace MichiSampleGame
+{
+    public static class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            using (Game game = new SampleGame())
+            using (GameHost host = Host.GetSuitableHost(@"sample-game"))
+                host.Run(game);
+        }
+    }
+}
---
+++ MichiSampleGame/MichiSampleGame/SampleGame.cs
@@ -0,0 +1,51 @@
+﻿using osu.Framework;
+using osu.Framework.Graphics;
+using OpenTK;
+using OpenTK.Graphics;
+using osu.Framework.Graphics.Shapes;
+using osu.Framework.Allocation;
+
+namespace MichiSampleGame
+{
+    internal class SampleGame : Game
+    {
+        private Box box;
+
+        private bool expansion = false;
+
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            Add(box = new Box
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                Size = new Vector2(150, 150),
+                Colour = Color4.Gold
+            });
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+            if (box.Size.X <= 30)
+            {
+                expansion = true;
+            }
+            if (box.Size.X >= 300)
+            {
+                expansion = false;
+            }
+
+            if (expansion)
+            {
+                box.Size += new Vector2(1, 1);
+            }
+            if (!expansion)
+            {
+                box.Size -= new Vector2(1, 1);
+            }
+            box.Rotation += (float)Time.Elapsed / 10;
+        }
+    }
+}
