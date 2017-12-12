fe2d38e093f638e73a1429f27ec85c5613469486
Michael Blank <michi2806@gmail.com>
2017-12-12 21:18:39 +0100

    New Stuff

--- MichiSampleGame/MichiSampleGame/SampleGame.cs
+++ MichiSampleGame/MichiSampleGame/SampleGame.cs
@@ -4,48 +4,76 @@ using OpenTK;
 using OpenTK.Graphics;
 using osu.Framework.Graphics.Shapes;
 using osu.Framework.Allocation;
+using osu.Framework.Graphics.UserInterface;
+using osu.Framework.Input;
+using osu.Framework.Graphics.Cursor;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Platform;
 
 namespace MichiSampleGame
 {
     internal class SampleGame : Game
     {
         private Box box;
-
-        private bool expansion = false;
+        private Button button;
+        InputState iS;
 
         [BackgroundDependencyLoader]
         private void load()
         {
-            Add(box = new Box
+            iS = new InputState();
+            Children = new Drawable[]
             {
-                Anchor = Anchor.Centre,
-                Origin = Anchor.Centre,
-                Size = new Vector2(150, 150),
-                Colour = Color4.Gold
-            });
+                new CursorContainer(),
+                box = new Box
+                {
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    Size = new Vector2(150, 150),
+                    Colour = Color4.Tomato
+                },
+                new FillFlowContainer
+                {
+                    Anchor = Anchor.TopLeft,
+                    Origin = Anchor.TopLeft,
+                    Direction = FillDirection.Vertical,
+                    Spacing = new Vector2(0, 10),
+                    Padding = new MarginPadding(10),
+                    AutoSizeAxes = Axes.Both,
+                    Children = new Drawable[]
+                    {
+                        new MyCheckbox(button)
+                        {
+                            LabelText = @"MyCheckbox",
+                        }
+                    }
+                }
+            };
+
+            button.Enabled.Value = true;
         }
 
         protected override void Update()
         {
-            base.Update();
-            if (box.Size.X <= 30)
-            {
-                expansion = true;
-            }
-            if (box.Size.X >= 300)
-            {
-                expansion = false;
-            }
 
-            if (expansion)
-            {
-                box.Size += new Vector2(1, 1);
-            }
-            if (!expansion)
-            {
-                box.Size -= new Vector2(1, 1);
-            }
+            base.Update();
             box.Rotation += (float)Time.Elapsed / 10;
         }
+
+        public override void SetHost(GameHost host)
+        {
+            base.SetHost(host);
+            host.Window.CursorState |= CursorState.Hidden;
+            host.Window.Title = "TestGameByMichl";
+        }
+
+        public class MyCheckbox : BasicCheckbox
+        {
+            public MyCheckbox(Button box)
+            {
+                Current.ValueChanged += v => box.Rotation += 10;
+            }
+        }
+
     }
 }
